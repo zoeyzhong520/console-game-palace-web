a0029a49bd8e4d21a30de63d8087d79b57b9f71c
Joe <zoeyzhong520@icloud.com>
2021-12-20 18:19:05 +0800

    搭建首页导航区域

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,52 +1,44 @@
 import Carousel from './carousel/carousel'
-import { tabsList } from './common'
 import './home.css'
 
-// 左侧菜单
-const Left = () => {
-    const logo = () => {
-        return <img className='home-left-logo' src={require('./static/logo.png').default} alt='' />
-    }
-
-    const showTabs = () => {
-        return <div>
-            {tabsList.map(item => {
-                return <li key={item}><a href='#'>{item.name}</a></li>
-            })}
-        </div>
-    }
-
-    return (
-        <div className='home-left'>
-            {logo()}
-            {showTabs()}
-        </div>
-    )
-}
-
-// 搜索框+广告位+列表
-const Content = () => {
-    // 搜索框
-    const searchBar = () => {
-        return <div className='home-content-search-bar'>
-            <input type='text' placeholder='请输入...' name='search'></input>
-            <button>搜索</button>
-        </div>
-    }
-
-    return (
-        <div className='home-content'>
-            {searchBar()}
-            <Carousel />
-        </div>
-    )
-}
-
 const Home = () => {
+    const Nav = () => {
+        return (
+            <div className='nav w'>
+                <img src={require('./static/logo.png').default} alt=''></img>
+                <div className="tab">
+                    <ul>
+                        <li><a href='#'>游戏推荐</a></li>
+                        <li><a href='#'>热门文章</a></li>
+                        <li><a href='#'>关于小助手</a></li>
+                        <li><a href='#'>加入俱乐部</a></li>
+                    </ul>
+                </div>
+                <div className="search">
+                    <input type="text" name='search' placeholder='请输入游戏名称' />
+                    <button>搜索</button>
+                </div>
+            </div>
+        )
+    }
+
+    const Main = () => {
+        return (
+            <div className='main w'>
+                <div className="hd">
+                    <div className="screening"></div>
+                    <div className="carousel">
+                        <Carousel />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
-        <div className='home'>
-            <Left />
-            <Content />
+        <div className='home w'>
+            <Nav />
+            <Main />
         </div>
     )
 }
